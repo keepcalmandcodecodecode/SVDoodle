50e3fdd8e92e6b77cc2a7ae3b2a3f42cd04a707b
igorbushin <ingvar.gorbushin@gmail.com>
2016-08-10 19:57:06 +0300

    commit with bug

--- SVDoodle/SVDoodle/AppDelegate.m
+++ SVDoodle/SVDoodle/AppDelegate.m
@@ -17,6 +17,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    CGRect windowFrame = [[UIScreen mainScreen] bounds];
+    UIWindow *theWindow = [[UIWindow alloc] initWithFrame:windowFrame];
+    [self setWindow:theWindow];
     return YES;
 }
 
